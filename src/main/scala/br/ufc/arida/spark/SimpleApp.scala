5c4c89ca9f8a489353944b6c4254a429edc5c8db
Igo Brilhante <brilhanteigo@gmail.com>
2014-10-31 15:25:59 -0300

    inputs

--- src/main/scala/br/ufc/arida/spark/SimpleApp.scala
+++ src/main/scala/br/ufc/arida/spark/SimpleApp.scala
@@ -7,24 +7,29 @@ package br.ufc.arida.spark
  * Time: 09:41
  */
 import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 object SimpleApp {
 
   def main(args: Array[String]) {
-    val logFile = "/tmp/example.txt" // Should be some file on your system
+    val logFile = args(0) // Should be some file on your system
+    val output = args(1)
 
     val conf = new SparkConf().setAppName("Simple Application")
 
     val sc = new SparkContext(conf)
 
-    val logData = sc.textFile(logFile, 2).cache()
+    val file = sc.textFile(logFile, 2)
 
-    val numAs = logData.filter(line => line.contains("a")).count()
+    // word count
+    val words = file.flatMap( line => line.split(" ") )
+                    .map( word => (word, 1) )
+                    .reduceByKey(_ + _)
+                    .map( (x : (String, Int))  => x._1 + "\t" + x._2)
+    // store the result
+    words.saveAsTextFile(output)
 
-    val numBs = logData.filter(line => line.contains("b")).count()
-
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
 
   }
 
